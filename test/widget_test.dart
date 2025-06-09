19deb011c9ae394fb2bc843b16d6a006285b5840
adnantaro <t4rotar0@gmail.com>
2025-06-09 19:29:44 +0700

    ubah nama dan icon app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:prices_project/main.dart';
+import 'package:prices_project_rpl5/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
